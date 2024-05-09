9d26761398278321a249d233a67b7563adc47f83
prathmesh-23 <indulkarprathmesh7@gmail.com>
2024-05-09 11:23:54 +0530

    Main Project Resource

---
+++ src/main/java/com/Wetherapplication/WetherApplication.java
@@ -0,0 +1,13 @@
+package com.Wetherapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Wetherapplication/WetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wetherapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
